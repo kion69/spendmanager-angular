635202b1f672ade433585907282c15c28c8fd476
Otavio Ornellas <Otavio Ornellas>
2021-05-23 03:07:27 -0300

    changed logic for add form on spent modal, added functions to calculate total spent, validade form,  changed html , changed style from spent modal

--- src/app/component/modal/add-spent/add-spent.component.ts
+++ src/app/component/modal/add-spent/add-spent.component.ts
@@ -1,7 +1,9 @@
-import { AfterViewInit, Component, ComponentRef, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, ComponentRef, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat'
 import { verticalSlideAnimation } from 'src/assets/animations/slide';
+
 
 @Component({
   selector: 'app-add-spent',
@@ -15,57 +17,104 @@ import { verticalSlideAnimation } from 'src/assets/animations/slide';
 export class AddSpentComponent implements OnInit, AfterViewInit {
 
   reference: ComponentRef<AddSpentComponent>;
-  showModal = false;
-  disableAnimation = true;
-  spentForm = [];
-  formLegal: FormGroup;
-  constructor(
-    private formBuilder: FormBuilder
-  ) {
-    this.formLegal = new FormGroup({});
+  showModal: boolean;
+  disableAnimation: boolean;
+  spentInformation = Array<any>();
+  spentForm: FormGroup;
+  addNewDate: boolean;
+  newDateInput: string;
+
+  constructor() {
+    this.newDateInput = '';
+    this.addNewDate = false;
+    this.showModal = false;
+    this.disableAnimation = true;
+
+    this.spentInformation = [{
+      spentDate: dayjs().format('DD/MM/YYYY'),
+      spentTotal: 0,
+      spentList: [
+        {
+          itemName: 'Cadeira',
+          itemValue: 123,
+          itemDescription: ''
+        },
+        {
+          itemName: 'Cafeteira',
+          itemValue: 12,
+          itemDescription: 'Magica'
+        },
+        {
+          itemName: 'Frigideira',
+          itemValue: 123,
+          itemDescription: 'Isso ae'
+        }
+      ]
+    }];
+
+    this.spentForm = new FormBuilder().group({
+      itemName: ['', Validators.required],
+      itemValue: ['', Validators.required],
+      itemDescription: ['']
+    });
   }
 
   ngOnInit(): void {
-    if (this.spentForm.length === 0) {
-
-      this.spentForm.push({
-        spentDate: dayjs().format('DD/MM/YYYY'),
-        spentTotal: 0,
-        spentItems: []
-      });
-    }
-  }
-
-  evento(event) {
-    console.log('legal');
-    const abc = new FormBuilder().group({
-      itemName: ['', Validators.required],
-      itemValue: ['', Validators.required],
-      itemDescription: [''],
-    });
-
-    this.spentForm[0].spentItems.push(abc);
-
-    console.log(this.spentForm);
-  }
-
-  addNewSpentForm(item) {
-    console.log('legal');
-    this.formLegal = new FormBuilder().group({
-      itemName: ['', Validators.required],
-      itemValue: ['', Validators.required],
-      itemDescription: [''],
-    });
-
-    item.spentItems.push(this.formLegal);
-
-    console.log(this.spentForm);
+    this.calculateTotal();
   }
 
   ngAfterViewInit(): void {
     setTimeout(() => {
       this.disableAnimation = false;
     });
+  }
+
+  calculateTotal() {
+    this.spentInformation
+      .forEach(spent => {
+        spent.spentTotal = spent.spentList.reduce((counter: any, currentValue: any) => counter + currentValue.itemValue, 0);
+      });
+  }
+
+  validateInput(currentList) {
+    const itemName = this.spentForm.controls['itemName'];
+    const itemValue = this.spentForm.controls['itemValue'];
+    const itemDescription = this.spentForm.controls['itemDescription'];
+
+    if (itemName.valid && itemValue.valid) {
+      currentList.spentList.push({
+        itemName: itemName.value,
+        itemValue: Number(itemValue.value),
+        itemDescription: itemDescription.value
+      });
+    }
+    this.spentForm.reset();
+    this.calculateTotal();
+  }
+
+  insertNewDate(input) {
+
+    if (input === undefined || input === '') {
+      this.addNewDate = !this.addNewDate;
+      return;
+    }
+
+    const date = input?.replace(/[-|\/]/g, '/').split('/');
+    const formattedDate = `${date[1]}/${date[0]}/${date[2]}`;
+
+    if (!dayjs(formattedDate, 'DD/MM/YYYY', true).isValid()) {
+      this.addNewDate = !this.addNewDate;
+      return;
+    }
+
+    this.spentInformation.push({
+      spentDate: dayjs(formattedDate).format('DD/MM/YYYY'),
+      spentTotal: 0,
+      spentList: []
+    });
+
+    this.newDateInput = undefined;
+    this.addNewDate = !this.addNewDate
   }
 
   animationDone(event: any) {
