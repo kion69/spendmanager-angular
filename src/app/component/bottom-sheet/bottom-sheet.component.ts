736c24da79bfd4f4094c2972befa84ad200343f6
Otavio Ornellas <Otavio Ornellas>
2021-05-30 22:36:47 -0300

    CHange some styles, added function to reset form of add-spent dialog, add styles to get full height

--- src/app/component/bottom-sheet/bottom-sheet.component.ts
+++ src/app/component/bottom-sheet/bottom-sheet.component.ts
@@ -1,25 +1,43 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatBottomSheetRef, MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
+import { BottomSheetActions } from 'src/app/enum/bottom-sheet';
+import { expandPanel } from 'src/assets/animations/expand';
 
 @Component({
   selector: 'app-bottom-sheet',
   templateUrl: './bottom-sheet.component.html',
-  styleUrls: ['./bottom-sheet.component.scss']
+  styleUrls: ['./bottom-sheet.component.scss'],
+  animations: [expandPanel]
 })
 export class BottomSheetComponent implements OnInit {
 
   spentItem;
+  openEdit = false;
+  editSpentForm: FormGroup;
 
   constructor(
     @Inject(MAT_BOTTOM_SHEET_DATA) public data: any,
-    private bottomSheetRef: MatBottomSheetRef) { }
+    private bottomSheetRef: MatBottomSheetRef) {
+    this.editSpentForm = new FormBuilder().group({
+      itemName: ['', Validators.required],
+      itemValue: ['', Validators.required],
+      itemDescription: ['']
+    });
+  }
 
   ngOnInit(): void {
     this.spentItem = this.data;
+
+
+
+    this.bottomSheetRef.backdropClick().subscribe(x => {
+      this.bottomSheetRef.dismiss({ action: -1 });
+    });
   }
 
-  close() {
-    this.bottomSheetRef.dismiss();
+  close(action: BottomSheetActions) {
+    this.bottomSheetRef.dismiss({ action: action });
   }
 
 }
--- src/app/component/modal/add-spent/add-spent.component.ts
+++ src/app/component/modal/add-spent/add-spent.component.ts
@@ -1,8 +1,9 @@
-import { AfterViewInit, Component, ComponentRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ComponentRef, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { MatDialog } from '@angular/material/dialog';
 import dayjs from 'dayjs';
+import { BottomSheetActions } from 'src/app/enum/bottom-sheet';
 import { DateParseService } from 'src/app/services/date-parse.service';
 import { verticalSlideAnimation } from 'src/assets/animations/slide';
 import { BottomSheetComponent } from '../../bottom-sheet/bottom-sheet.component';
@@ -17,25 +18,26 @@ import { BottomSheetComponent } from '../../bottom-sheet/bottom-sheet.component'
     '(@verticalSlideAnimation.done)': 'animationDone($event)'
   }
 })
-export class AddSpentComponent implements OnInit, AfterViewInit {
+export class AddSpentComponent implements OnInit {
 
   dateForm: FormGroup;
   spentInformation = Array<any>();
   disableAnimation: boolean;
   addNewDate: boolean;
   newDateInput: string;
-
-  teste: any;
+  editingAction: boolean;
+  editingItem: any;
 
   @ViewChild('picker') picker;
+  @ViewChild('form') form: ElementRef;
 
   constructor(
     private dateParser: DateParseService,
-    private dialog: MatDialog,
     private bottomSheet: MatBottomSheet) {
     this.newDateInput = '';
     this.addNewDate = false;
     this.disableAnimation = true;
+    this.editingAction = false;
 
     this.dateForm = new FormBuilder().group({
       newDateInput: ['']
@@ -127,12 +129,6 @@ export class AddSpentComponent implements OnInit, AfterViewInit {
     this.calculateTotal();
   }
 
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.disableAnimation = false;
-    });
-  }
-
   calculateTotal() {
     this.spentInformation
       .forEach(spent => {
@@ -147,12 +143,14 @@ export class AddSpentComponent implements OnInit, AfterViewInit {
 
     if (itemName.valid && itemValue.valid) {
       currentList.spentList.push({
+        id: Math.ceil(Math.random() * 100),
         itemName: itemName.value,
         itemValue: Number(itemValue.value),
         itemDescription: itemDescription.value
       });
     }
     currentList.spentForm.reset();
+    this.editingAction = false;
     this.calculateTotal();
   }
 
@@ -180,8 +178,7 @@ export class AddSpentComponent implements OnInit, AfterViewInit {
       spentList: []
     });
 
-    this.picker.close();
-    this.dateForm.controls['newDateInput'].setErrors(null);
+    // this.picker.close();
     this.dateForm.reset();
   }
 
@@ -190,13 +187,48 @@ export class AddSpentComponent implements OnInit, AfterViewInit {
     this.calculateTotal();
   }
 
-  editItem(item, list, index) {
-
+  editItem(item, spentList, spentForm: FormGroup) {
+    spentForm.controls['itemName'].setValue(item.itemName);
+    spentForm.controls['itemValue'].setValue(item.itemValue);
+    spentForm.controls['itemDescription'].setValue(item.itemDescription);
   }
 
-  openBagulho(spentItem){
+  saveItem(list, form: FormGroup, abc, i) {
+    const { index } = this.editingItem;
+    list[index] = {
+      itemName: form.controls['itemName'].value,
+      itemValue: form.controls['itemValue'].value,
+      itemDescription: form.controls['itemDescription'].value
+    }
+    this.calculateTotal();
+  }
+
+  clearForm(form: FormGroup) {
+    form.reset();
+    this.editingAction = !this.editingAction;
+  }
+
+  openOptions(spentItem, spentList, spentForm, index) {
     this.bottomSheet.open(BottomSheetComponent, {
-      data: spentItem
+      data: spentItem,
+    }).afterDismissed().subscribe(result => {
+
+      const { action } = result;
+
+      switch (action) {
+        case BottomSheetActions.EDIT_ITEM:
+          (this.form.nativeElement as HTMLElement).scrollTop = 999;
+          this.editingAction = true;
+          this.editingItem = { spentItem, spentList, spentForm, index }
+          this.editItem(spentItem, spentList, spentForm);
+          break;
+
+        case BottomSheetActions.DELETE_ITEM:
+          this.deleteItem(spentList, index);
+          break;
+        default:
+          break;
+      }
     });
   }
 }
